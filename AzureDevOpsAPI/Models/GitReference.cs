7f9082074dc439395cea4b9ebca7267db062edca
Bette, Robin <robin.bette@capgemini.com>
2020-08-20 16:46:35 +0200

    addProject

---
+++ AzureDevOpsAPI/Models/GitReference.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureDevOpsAPI.Models
+{
+    public class GitReference
+    {
+        public string name { get; set; }
+        public string objectId { get; set; }
+        public GitReferenceCreator creator { get; set; }
+        public string url { get; set; }
+    }
+}
